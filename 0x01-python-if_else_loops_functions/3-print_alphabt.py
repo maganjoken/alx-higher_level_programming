f31badbaf3867808389731b5282bbdb3a342c3f0
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-11-29 15:57:10 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
-=======
 #!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
->>>>>>> 55f87e6d3c8b417cc5ff2f9431b18864f6320c6b
