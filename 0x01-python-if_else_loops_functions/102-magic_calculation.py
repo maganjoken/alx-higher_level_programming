c2c94a74cbda9968755f2c5aa25af951ed037933
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-11-29 18:47:28 +0300

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,1 +1,12 @@
-erdfgv
+#!/usr/bin/python3
+# 102-magic_calculation.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
