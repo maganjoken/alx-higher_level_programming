4d52aaaeb5424524160a10987475a2363f4931a1
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-12-01 15:12:30 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
-    print("{:d}".format(a))
-    
+from variable_load_5 import a
+
+if __name__ != "__main__":
+    exit()
+
+print("{:d}".format(a))
