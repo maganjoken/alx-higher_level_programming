3c627b669a346f21867d15fd59a013ca9d5d9569
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-12-03 17:33:05 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,7 @@
-sdfgh
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in range(len(row)):
+            print("{:d}".format(row[col]),
+                  end=' ' if col < len(row) - 1 else '')
+        print()
