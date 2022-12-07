91ecf945ffb5c526a5bbacd96ddeb81585e4bbef
maganjoken <111058600+maganjoken@users.noreply.github.com>
2022-12-07 23:23:08 +0300

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,6 @@
-dfghj
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for idx in list(a_dictionary.keys()):
+        if a_dictionary[idx] == value:
+            del a_dictionary[idx]
+    return a_dictionary
