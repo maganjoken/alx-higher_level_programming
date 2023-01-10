67cb2bcca897586b07022ced1d1eb29b1e521619
maganjoken <111058600+maganjoken@users.noreply.github.com>
2023-01-10 22:48:55 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,40 +1,14 @@
 #!/usr/bin/python3
-"""
-=============================
-Module with a class Myint
-=============================
-"""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """class that inherits from int"""
+    """Invert int operators == and !=."""
 
-    def __init__(self, my_int):
-        """for initialize a value my_int"""
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-        self.my_int = my_int
-
-    @property
-    def my_int(self):
-        return self.__my_int
-
-    @my_int.setter
-    def my_int(self, my_int):
-        if type(my_int) is not int:
-            raise TypeError("my_int must be an integer")
-        else:
-            self.__my_int = my_int
-
-    def __eq__(self, other):
-        """equal method"""
-
-        if self.my_int == other:
-            return False
-        else:
-            return True
-
-    def __ne__(self, other):
-            if self.my_int != other:
-                return False
-            else:
-                return True
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
